c631a45ad6c7ec75095e547715b4f395b0f6b52f
cha-suyeon <suyeon.chaa@gmail.com>
2021-09-10 00:53:04 +0900

    upload Jump 21

--- Jump_to_python_BuiltInfunction/isintance.py
+++ Jump_to_python_BuiltInfunction/isintance.py
@@ -1,4 +1,4 @@
-# isintance(object, class), 첫 번쨰 인수로 인스턴스, 두 번째 인수로 클래스 이름을 받는다.
+# isinstance(object, class), 첫 번째 인수로 인스턴스, 두 번째 인수로 클래스 이름을 받는다.
 # 입력으로 받은 인스턴스가 그 클래스의 인스턴스인지를 판단하여 참이면 True, 거짓이면 False
 
 class Person: pass # 아무 기능 없는 person 클래스 생성
