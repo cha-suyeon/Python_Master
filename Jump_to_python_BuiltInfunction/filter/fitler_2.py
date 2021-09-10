e137bf4fce2052a4da5839d75372f04cc74bfe2d
cha-suyeon <suyeon.chaa@gmail.com>
2021-09-10 22:40:26 +0900

    upload Jump 21

--- Jump_to_python_BuiltInfunction/filter/fitler_2.py
+++ Jump_to_python_BuiltInfunction/filter/fitler_2.py
@@ -3,5 +3,5 @@
 def positive(x):
     return x > 0
 
-print(list(filter(positive, [1,-3,-2,0,-5,6])))
+print(list(filter(positive, [1,-3,2,0,-5,6])))
 
---
+++ map.py
@@ -0,0 +1,9 @@
+line = [[('survived', '0'), ('sex', 'male'), ('age', '22.0'), ('n_siblings_spouses', '1'),
+('parch', '0'), ('fare', '7.25'),  ('class', 'Third'), ('deck', 'unknown'), ('embark_town', 'Southampton'), ('alone', 'n')],
+[('survived', '1'), ('sex', 'female'),('age', '38.0'), ('n_siblings_spouses', '1'),('parch', '0'),('fare', '71.2833'),
+('class', 'First'), ('deck', 'C'), ('embark_town', 'Cherbourg'), ('alone', 'n')],
+[('survived', '1'),('sex', 'female'),('age', '26.0'),('n_siblings_spouses', '0'),('parch', '0'),('fare', '7.925'),
+('class', 'Third'),('deck', 'unknown'),('embark_town', 'Southampton'), ('alone', 'y')]]
+print(line)
+
+print(line[0][1], line[2][1])
