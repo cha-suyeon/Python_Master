7a7a1be1ceba949964cbb305d0e9e1700ce7406a
cha-suyeon <suyeon.chaa@gmail.com>
2021-09-15 09:20:48 +0900

    upload Jump 11

--- Jump_to_python_while문/while_1.py
+++ Jump_to_python_while문/while_1.py
@@ -1,10 +1,14 @@
 # while문의 기본 구조
+
+import time
 treeHit = 0
 while treeHit < 10:
     treeHit = treeHit + 1
     print("나무를 %d번 찍었습니다." % treeHit)
+    time.sleep(1)
     if treeHit == 10:
         print("나무 넘어갑니다.")
+        
 
 # while의 조건문은 treeHit < 10입니다.
 # treeHit가 10보다 작을 동안은 while문은 print 안의 문장을 계속 출력합니다!
