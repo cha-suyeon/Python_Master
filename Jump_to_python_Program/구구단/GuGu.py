903cf6e7e974d4f19c92b04f057c22bf9424d21c
cha-suyeon <suyeon.chaa@gmail.com>
2021-09-15 09:20:24 +0900

    upload Jump 24

--- Jump_to_python_Program/구구단/GuGu.py
+++ Jump_to_python_Program/구구단/GuGu.py
@@ -9,3 +9,13 @@ def GuGu(n):
     return result
 
 print(GuGu(2))
+
+def gugu(n):
+    result = []
+    i = 1
+    while i < 10:
+        result.append(n*i)
+        i += 1
+    return result
+
+print(gugu(3))
